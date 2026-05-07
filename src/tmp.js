1ac5d92e6c42fd022aefabdfd2815c7ac175ba1c
KM <the.real.laplace@gmail.com>
2026-05-07 21:07:27 +0300

    chore(commit): force

--- src/tmp.js
+++ src/tmp.js
@@ -3,10 +3,13 @@ const Fs = require('node:fs')
 const Os = require('node:os')
 const Path = require('node:path')
 
-const _prefix = Path.join(Os.tmpdir(), `logs-${process.pid}-`)
+const _name = Path.basename(process.argv[0])
+const _file = `${_name}-${process.pid}-`
+const _prefix = Path.join(Os.tmpdir(), 'log', _file)
 const _path = Fs.mkdtempSync(_prefix)
-console.warn(`logging to ${_path}`)
 const _dir = new Dir(_path)
+
+console.warn(`logging to ${_path}`)
 
 class TmpLogger {
 	constructor (name, props) {
